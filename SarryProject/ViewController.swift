1b1bbae45e34a5563b2801b3d3961a0383253e5b
Umair khan <ktrading.9430@gmail.com>
2021-09-13 21:13:54 +0300

    Initial Commit

---
+++ SarryProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SarryProject
+//
+//  Created by omair khan on 13/09/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
